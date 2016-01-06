70e43927548e9041b29e6c7c5e47ef379b85cabb
Kevin Nguyen <threekevinnguyen@gmail.com>
2016-01-05 23:35:42 -0800

    Remap personal layout

--- keyboard/planck/keymaps/keymap_kvn.c
+++ keyboard/planck/keymaps/keymap_kvn.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC},
   {KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH},
   {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT },
-  {KC_LCTL, KC_LGUI, KC_LALT, TG(_LW1), TG(_LW2), KC_SPC, KC_SPC, MO(_RS2), MO(_RS1), KC_CAPS, M(0),  KC_DELT}
+  {KC_LCTL, KC_LGUI, KC_LALT, TG(_LW1), MO(_RS1), KC_SPC, KC_SPC, MO(_RS2), TG(_LW2), KC_CAPS, M(0),  KC_DELT}
 },
 [_RS1] = { /* RAISE 1 */
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
@@ -54,10 +54,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,  KC_SPC,  KC_SLSH, KC_ASTR, KC_MINS, KC_PLUS, KC_EQL}
 },
 [_LW2] = { /* LOWER 2 */
-  {DF(_QW)}, F(10),  F(11),   F(12),   F(13),   F(14),   KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MPLY},
-  {DF(_CM)}, F(15),  F(16),   F(17),   F(18),   F(19),   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_HOME, KC_PGUP},   
+  {DF(_QW), F(10),  F(11),   F(12),   F(13),   F(14),   KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MPLY},
+  {DF(_CM), F(15),  F(16),   F(17),   F(18),   F(19),   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_HOME, KC_PGUP},   
   {DF(_DV),  F(20),  F(21),   F(22),   F(23),   F(24),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_END,  KC_PGDOWN},
-  {KC_LCTL, KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS, RESET,   RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_PSCR}
+  {KC_LCTL, KC_LSFT, KC_LALT, KC_TRNS, KC_TRNS, RESET,   RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_PSCR}
 }
 };
 
